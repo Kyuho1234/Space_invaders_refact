1c7cdf3e9ef39f0e287b6db29946f89a463c74be
swimmerRAt <swim@jbnu.ac.kr>
2025-11-15 20:18:04 +0900

    ResourceManager.java, Assets.java 파일 생성

---
+++ src/main/resources/Assets.java
@@ -0,0 +1,10 @@
+package org.newdawn.spaceinvaders.resources;
+
+public class Assets {
+    public static final String ALIEN = "/sprites/alien.png";
+    public static final String PLAYER = "/sprites/player.png";
+    public static final String SHOT = "/sprites/shot.png";
+
+    public static final String SOUND_EXPLOSION = "/sounds/explosion.wav";
+    public static final String SOUND_FIRE = "/sounds/fire.wav";
+}
---
+++ src/main/resources/ResourceManager.java
@@ -0,0 +1,26 @@
+package org.newdawn.spaceinvaders.resources;
+
+import javax.imageio.ImageIO;
+import java.awt.Image;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResourceManager {
+
+    private static final Map<String, Image> imageCache = new HashMap<>();
+
+    public static Image loadImage(String path) {
+        if (imageCache.containsKey(path)) {
+            return imageCache.get(path);
+        }
+
+        try {
+            Image img = ImageIO.read(ResourceManager.class.getResource(path));
+            imageCache.put(path, img);
+            return img;
+        } catch (IOException | IllegalArgumentException e) {
+            throw new RuntimeException("Image load failed: " + path, e);
+        }
+    }
+}
